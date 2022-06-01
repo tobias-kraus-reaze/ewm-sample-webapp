0afd4f6e3cfd219d4f0c4850b8f3aab65cff0f2d
Tobias Kraus <tobias.kraus@reaze.com>
2022-06-01 10:32:31 +0200

    feat: add events service and events queries

---
+++ src/app/graphql/queries/events.queries.ts
@@ -0,0 +1,90 @@
+import { gql } from "apollo-angular";
+
+const eventProperties = `
+  id
+  date
+  topline
+  headline
+  slug
+  timeStart
+  timeEnd
+  eventLanguage
+  eventFormatValue
+  eventFormatOptionsProvider {
+    ... on object_OptionsProvider {
+      id
+      label
+    }
+  }
+  eventTopicValue
+  eventTopicOptionsProvider {
+    ... on object_OptionsProvider {
+      id
+      label
+    }
+  }
+  eventTypeValue
+  eventTypeOptionsProvider {
+    ... on object_OptionsProvider {
+      id
+      label
+    }
+  }
+  tags {
+    ... on element_tag {
+      name
+    }
+  }
+  pageGql: page {
+    ... on document_page {
+      fullpath
+    }
+  }
+  image {
+    ... on hotspotimage {
+      image {
+        fullpath
+      }
+    }
+  }
+  video {
+    ... on Video {
+      data {
+        ... on asset {
+          filename
+          type
+          mimetype
+          parent {
+            ... on asset_folder {
+              fullpath
+            }
+          }
+        }
+      }
+      poster {
+        fullpath
+      }
+    }
+  }
+  location {
+    ... on object_Location {
+      name
+    }
+  }
+`
+
+export const GET_EVENT_LISTING = gql`
+  query getEventListing(
+    $appId: String!
+    $lang: String
+    $numberOfEvents: Int
+  ) {
+    getEventListing(sortBy: "date", first: $numberOfEvents, sortOrder: "DESC", appId: $appId, defaultLanguage: $lang) {
+      edges {
+        node {
+          ${eventProperties}
+        }
+      }
+    }
+  }
+`;
--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { GET_EVENT_LISTING } from '../graphql/queries/events.queries';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
+
+  public getEventListing(): Observable<any> {
+    return this.apollo.query({
+      query: GET_EVENT_LISTING,
+      variables: {
+        appId: 'experience',
+        lang: 'de',
+        numberOfEvents: 25
+      }
+    }).pipe(
+      map((response: any) => response.data.getEventListing.edges.map(eventEdge => eventEdge.node)),
+      catchError((error: any) => throwError(error.message))
+    );
+  }
+
 }
