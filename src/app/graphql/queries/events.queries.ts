91ce0bac0114a3f9991e3393a85fd5590a9a19ce
Tobias Kraus <tobias.kraus@reaze.com>
2022-06-01 13:30:49 +0200

    feat: move event properties to qgl function

--- src/app/graphql/queries/events.queries.ts
+++ src/app/graphql/queries/events.queries.ts
@@ -1,78 +1,5 @@
 import { gql } from "apollo-angular";
 
-const eventProperties = `
-  id
-  date
-  topline
-  headline
-  slug
-  timeStart
-  timeEnd
-  eventLanguage
-  eventFormatValue
-  eventFormatOptionsProvider {
-    ... on object_OptionsProvider {
-      id
-      label
-    }
-  }
-  eventTopicValue
-  eventTopicOptionsProvider {
-    ... on object_OptionsProvider {
-      id
-      label
-    }
-  }
-  eventTypeValue
-  eventTypeOptionsProvider {
-    ... on object_OptionsProvider {
-      id
-      label
-    }
-  }
-  tags {
-    ... on element_tag {
-      name
-    }
-  }
-  pageGql: page {
-    ... on document_page {
-      fullpath
-    }
-  }
-  image {
-    ... on hotspotimage {
-      image {
-        fullpath
-      }
-    }
-  }
-  video {
-    ... on Video {
-      data {
-        ... on asset {
-          filename
-          type
-          mimetype
-          parent {
-            ... on asset_folder {
-              fullpath
-            }
-          }
-        }
-      }
-      poster {
-        fullpath
-      }
-    }
-  }
-  location {
-    ... on object_Location {
-      name
-    }
-  }
-`
-
 export const GET_EVENT_LISTING = gql`
   query getEventListing(
     $appId: String!
@@ -82,7 +9,76 @@ export const GET_EVENT_LISTING = gql`
     getEventListing(sortBy: "date", first: $numberOfEvents, sortOrder: "DESC", appId: $appId, defaultLanguage: $lang) {
       edges {
         node {
-          ${eventProperties}
+          id
+          date
+          topline
+          headline
+          slug
+          timeStart
+          timeEnd
+          eventLanguage
+          eventFormatValue
+          eventFormatOptionsProvider {
+            ... on object_OptionsProvider {
+              id
+              label
+            }
+          }
+          eventTopicValue
+          eventTopicOptionsProvider {
+            ... on object_OptionsProvider {
+              id
+              label
+            }
+          }
+          eventTypeValue
+          eventTypeOptionsProvider {
+            ... on object_OptionsProvider {
+              id
+              label
+            }
+          }
+          tags {
+            ... on element_tag {
+              name
+            }
+          }
+          pageGql: page {
+            ... on document_page {
+              fullpath
+            }
+          }
+          image {
+            ... on hotspotimage {
+              image {
+                fullpath
+              }
+            }
+          }
+          video {
+            ... on Video {
+              data {
+                ... on asset {
+                  filename
+                  type
+                  mimetype
+                  parent {
+                    ... on asset_folder {
+                      fullpath
+                    }
+                  }
+                }
+              }
+              poster {
+                fullpath
+              }
+            }
+          }
+          location {
+            ... on object_Location {
+              name
+            }
+          }
         }
       }
     }
