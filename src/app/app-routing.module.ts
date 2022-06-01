3257c5b28422574c3d5b2818bb27d69af6a71bde
Tobias Kraus <tobias.kraus@reaze.com>
2022-06-01 08:29:27 +0200

    feat: add second component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './views/first/first.component';
+import { SecondComponent } from './views/second/second.component';
 
 const routes: Routes = [
   { path: 'first', component: FirstComponent },
+  { path: 'second', component: SecondComponent },
 ];
 
 @NgModule({
