50c5886f872d4e8691baed964193a9ff1e52ea52
Tobias Kraus <tobias.kraus@reaze.com>
2022-06-01 08:45:47 +0200

    feat: ng add apollo-angular

---
+++ src/app/graphql.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
+import {HttpLink} from 'apollo-angular/http';
+
+const uri = 'https://d-ewm-3gg.xava.cloud/pimcore-graphql-webservices/Event?apikey=FF4EF4245DA5B09786E3D3DE8B430292FA081984DB272D2B13ED404B45353D28';
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+  return {
+    link: httpLink.create({uri}),
+    cache: new InMemoryCache(),
+  };
+}
+
+@NgModule({
+  exports: [ApolloModule],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: createApollo,
+      deps: [HttpLink],
+    },
+  ],
+})
+export class GraphQLModule {}
